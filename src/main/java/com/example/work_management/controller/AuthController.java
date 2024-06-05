d75323c4ae369d77b2701059938d3bf06fec9445
Chung Nguyen Thanh <chungnt2@officevnpay.com>
2024-06-05 18:27:55 +0700

    them @CrossOrigin cho phep web goi tu ten mien khac

--- src/main/java/com/example/work_management/controller/AuthController.java
+++ src/main/java/com/example/work_management/controller/AuthController.java
@@ -22,10 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -33,6 +30,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     Logger logger = LoggerFactory.getLogger(AuthController.class);
